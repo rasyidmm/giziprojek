897262d65b247ef18680148b881847f6c35dfbf7
rasyidmm <37688419+rasyidmm@users.noreply.github.com>
2019-04-13 10:40:25 +0700

    trykk

--- src/main/java/com/website/gizi/repository/KegiatanRepository.java
+++ src/main/java/com/website/gizi/repository/KegiatanRepository.java
@@ -12,9 +12,9 @@ import java.util.List;
 
 @Repository
 public interface KegiatanRepository extends JpaRepository<Kegiatan,Long> {
-    @Query(value = "select * from kegiatan a left join kegiatan_rekapitulasi b on a.id = b.kegiatan_id where b.rekapitulasi_id =:id ",nativeQuery = true)
+    @Query(value = "SELECT * from kegiatan a left join kegiatan_rekapitulasi b on a.id = b.kegiatan_id where b.rekapitulasi_id =:id ",nativeQuery = true)
     public List<Kegiatan> findKegiatanByRekapitulasiID(@Param("id")long id);
-    @Query(value = "select * from kegiatan a full join kegiatan_rekapitulasi b on a.id = b.kegiatan_id where a.tanggal_kegiatan =:waktu and b.rekapitulasi_id !=:id order by a.id desc",nativeQuery = true)
+    @Query(value = "SELECT * from kegiatan a full join kegiatan_rekapitulasi b on a.id = b.kegiatan_id where a.tanggal_kegiatan =:waktu and b.rekapitulasi_id !=:id order by a.id desc",nativeQuery = true)
     public List<Kegiatan> findKegiatanNONByRekapitulasiID(@Param("id")long id,@Param("waktu")String waktu);
     @Query(value = "SELECT * FROM kegiatan a LEFT JOIN target_penilaian b on a.id = b.kegiatan_id WHERE b.rekapitulasi_id =:idr ISNULL",nativeQuery = true)
     public List<Kegiatan>findKegiatanNONByTargetPenilaian(@Param("idr")long idr);
